7783b9bbdcfa131d42a6b4075d067e5428de0bcb
y0092888 <ju-horn@web.de>
2020-12-21 12:13:18 +0100

    main layout money game

--- src/App.js
+++ src/App.js
@@ -2,22 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const money = 3000;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="greeting">Hi Justin!</h3>
+      <h1 className="money">
+        You have <span>{money}€</span> to much! <br />
+        Send money to others!
+      </h1>
+
+      <div className="lists">
+        <div className="users">
+          <ul className="userlist">
+            <li>
+              <button>
+                <span> Name1</span> <span> Money</span>
+              </button>
+            </li>
+            <li>
+              <button>
+                <span> Name2</span> <span> Money</span>
+              </button>
+            </li>
+            <li>
+              <button>
+                <span> Name3</span> <span> Money</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="transactions">
+          <ul className="transaction-list">
+            <li className="sendMoney">
+              <span> You send X Y money</span>
+            </li>
+            <li className="recievedMoney">
+              <span> X send you Y money</span>
+            </li>
+            <li className="sendMoney">
+              <span> You send X Y money</span>
+            </li>
+            <li className="recievedMoney">
+              <span> X send you Y money</span>
+            </li>
+            <li className="recievedMoney">
+              <span> X send you Y money</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'justin-money.firebaseapp.com',
+  projectId: 'justin-money',
+  storageBucket: 'justin-money.appspot.com',
+  messagingSenderId: '877189545041',
+  appId: '1:877189545041:web:e98954b369ecda6e048c70',
+  measurementId: 'G-4MKTEQHET3',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
